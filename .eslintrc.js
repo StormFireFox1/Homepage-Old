6297fabfcd0f56d9492a31703bb97662772fc35a
Matei-Alexandru Gardus <matei@gard.us>
2021-05-19 01:45:35 -0700

    Initialize Next.js app
    This does some manual work of adding TypeScript in there, too.

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    // We will use TypeScript's types for component props instead
+    'react/prop-types': 'off',
+    // No need to import React when using Next.js
+    'react/react-in-jsx-scope': 'off',
+    // This rule is not compatible with Next.js's <Link /> components
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    // Why would you want unused vars?
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
+};
