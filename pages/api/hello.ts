739c8e6c4a082aca095f3ae44d02a5b0f2e1ab64
Matei-Alexandru Gardus <matei@gard.us>
2021-06-15 16:17:56 -0700

    Fix linting

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
+const helloRoute = (req, res) => {
   res.status(200).json({ name: 'John Doe' });
 };
+
+export default helloRoute;
