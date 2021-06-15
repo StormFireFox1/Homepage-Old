3bac421f1fceaabb3e187c1f74c180a58caae087
Matei-Alexandru Gardus <matei@gard.us>
2021-06-15 16:17:44 -0700

    Add lint and Tailwind JIT mode

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+            <meta name="description" content="A personal webpage. Now in Next.js, because why not." />
+            <link rel="icon" href="/favicon.ico" />
+            <link href="https://fonts.googleapis.com/css?family=Exo&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
