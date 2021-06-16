f44aa987d1b911e4cea8f41731cdf51d3445d58b
Matei-Alexandru Gardus <matei@gard.us>
2021-06-15 20:50:50 -0700

    Adjust Tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   mode: 'jit',
   purge: [
-    './pages/*.{js,ts,jsx,tsx}',
-    './pages/404.tsx',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/*.tsx',
+    './pages/**/*.tsx',
+    './components/*.tsx',
+    './components/**/*.tsx',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
