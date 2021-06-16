f1bca4e78f9f138b7bf443bab743e7890b6e7d8a
Matei-Alexandru Gardus <matei@gard.us>
2021-06-15 18:53:26 -0700

    Add all favicon files

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,13 @@ class MyDocument extends Document {
             <meta name="description" content="A personal webpage. Now in Next.js, because why not." />
             <link rel="icon" href="/favicon.ico" />
             <link href="https://fonts.googleapis.com/css?family=Exo&display=swap" rel="stylesheet" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#d55b5b" />
+            <meta name="msapplication-TileColor" content="#b91d47" />
+            <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
