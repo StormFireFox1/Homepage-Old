59f72baf06a9ba402280647a9b48dd6a3fec4dff
Matei-Alexandru Gardus <matei@gard.us>
2021-06-15 17:11:02 -0700

    Add Skill Card section

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,6 +1,8 @@
 import { DateTime } from "luxon";
 import { Ref } from "react";
+
 import Header from 'components/Header';
+import SkillCardGallery from 'components/SkillCardGallery';
 
 interface AboutMeProps {
   aboutMeRef: Ref<HTMLElement>;
@@ -13,7 +15,7 @@ const AboutMe = (props: AboutMeProps) => {
   const age = birthday.until(DateTime.now()).length('years').toFixed(0);
   return (
     <>
-      <main ref={aboutMeRef} className="w-screen max-w-4xl min-h-screen h-24 mx-auto my-auto p-8 max-h-96 font-exo">
+      <main ref={aboutMeRef} className="w-screen max-w-4xl min-h-screen h-24 mx-auto my-auto pt-8 max-h-96 font-exo">
         <p className="pt-5">I&apos;m a {age}-year-old student at UC San Diego, and I <i>really</i> like computers. Maybe too much.</p>
         <p className="py-2">It started with video games, but evolved into spending time tinkering with computers, exploding
           transistors, and messing with Vim configs. Now I mostly spend time on actual productive side-projects
@@ -21,6 +23,7 @@ const AboutMe = (props: AboutMeProps) => {
           computers I could find online or not.</p>
         <p className="py-2">Excluding computers, there are many other things I do as well, all while funneling life through my unique prism of humour and optimism.</p>
         <Header text="What I know" />
+        <SkillCardGallery />
       </main>
     </>
   );
---
+++ components/SkillCardGallery.tsx
@@ -0,0 +1,45 @@
+import SkillCard from 'components/SkillCard';
+
+interface Skill {
+    title: string;
+    content: string;
+};
+
+const skills: Skill[] = [
+    {
+        title: "Programming Languages",
+        content: "I know a few. Their full extent can be found on my GitHub repo \"HelloWorld\".",
+    },
+    {
+        title: "Machine Learning",
+        content: "Can't wait to automate more aspects of my life with that.",
+    },
+    {
+        title: "Some electrical engineering",
+        content: "Turns out you need hardware to make the software work."
+    },
+    {
+        title: "Intermediate computer algorithms",
+        content: "3 years of Romanian high school ought to do that to you.",
+    },
+    {
+        title: "Git",
+        content: "I have been told I'm good at Git and its commit messages by exactly one other person.",
+    },
+    {
+        title: "Life skills",
+        content: "Yes, that's important, too. I actually enjoy the simple minutiae of life, like cooking!",
+    },
+];
+
+const SkillCardGallery = () => {
+    return (
+        <div className="grid grid-flow-row grid-rows-2 grid-cols-3">
+            {skills.map((skill, index) => (
+                <SkillCard key={`skill-${index}`} title={skill.title} content={skill.content} />
+            ))}
+        </div>
+    )
+};
+
+export default SkillCardGallery;
